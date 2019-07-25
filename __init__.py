7bb765c5f54bf8fbbf007f2f67f849f4de9c4edc
AlexBdx <alexandre.bdx@gmail.com>
2019-07-25 13:03:54 -0400

    Test

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
-name = "video-tools"
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+__name__ = "videotools"
+__version__ = "0.0.6"
 
 """[Removed for packaging]
 import init
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="videotools",
-    version="0.0.4",
+    version="0.0.5",
     author="Alex Bondoux",
     author_email="alexandre.bdx@gmail.com",
     description="A collection of helper functions to process videos.",
