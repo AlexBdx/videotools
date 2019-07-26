78c7697119bb0bde735b69beafaba9ee8ae61e23
AlexBdx <alexandre.bdx@gmail.com>
2019-07-26 05:40:33 -0400

    Fixed issues with the imports

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ modules = glob.glob(path_glob)
 #valid_modules = [mod for mod in modules if mod[-3:] == '.py']
 module_blacklist = ['__init__.py', 'setup.py']
 __name__ = "videotools"
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
 print("[INFO] Imported", __all__)
 
